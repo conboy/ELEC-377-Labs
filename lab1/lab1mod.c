068f70b90d968cccd2142d9e6fb499ac7dafcb61
David Hoernke <20dash@queensu.ca>
2022-09-20 16:29:31 -0400

    updated headers, mapped ops for proc_ops

--- lab1/lab1mod.c
+++ lab1/lab1mod.c
@@ -2,9 +2,9 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
-#include <linux/sched.h>
+#include <linux/sched.h> // include sched.h to implement process control block
 #include <linux/proc_fs.h>
-#include <linux/seq_file.h>\
+#include <linux/seq_file.h>
 #include <linux/ktime.h>
 
 
@@ -13,25 +13,34 @@
 #endif
 
 static int lab1_show(struct seq_file *m, void *v) {
-  int hrs,mins,secs;
+  
   /* some code here */
   return 0;
 }
 
 static int lab1_open(struct inode *inode, struct  file *file) {
-  return single_open(file, /* some code here */lab1_show, NULL);
+  return single_open(file,lab1_show, NULL);
 }
 
 #ifdef HAVE_PROC_OPS
-static const struct proc_ops lab1_fops = {  // For newer Kernels 
+static const struct proc_ops lab1_fops = { 
+  .proc_open = lab1_open,
+  .proc_read = seq_read,
+  .proc_lseek = seq_lseek,
+  .proc_release = single_release,
+
+   // For newer Kernels 
   // initializing proc data structure, gets information from 
   /* operation mapping */
 };
 #else
 static const struct file_operations lab1_fops = {
-  .owner = THIS_MODULE;
-  .open = lab1_open;    //not sure what this is... yet. Only compiled if proc_ops doesn't compile
-  .read = seq_read;
+  .owner = THIS_MODULE,
+  .open = lab1_open,
+  .read = seq_read,
+  .llseek = seq_lseek,
+  .release = single_release,        //done
+
   
   /* some code here */
 };
@@ -45,8 +54,7 @@ static int __init lab1_init(void) {
 }
 
 static void __exit lab1_exit(void) {
-  remove_proc_entry("lab1", NULL)
-  /* remove proc entry --done */
+  remove_proc_entry("lab1", NULL);      // Remove proc entry
   printk(KERN_INFO "lab1mod out\n");
 }
 
